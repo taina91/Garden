d6b36c07d155ec915b3f4dd30ec767426be12f7d
taina91 <43749752+taina91@users.noreply.github.com>
2024-06-27 11:34:30 +0300

    Update Garden.js

--- src/models/Garden.js
+++ src/models/Garden.js
@@ -1,45 +1,3 @@
-// const Tree = require("./Tree");
-
-// class Garden {
-//   constructor(cntTree = 0, applesPerTree = cntTree * 2) {
-//     this.trees = [];
-//     this.age = 0;
-
-//     for (let i = 0; i < cntTree; i++) {
-//       const tree = new Tree();
-//       for (let j = 0; j < applesPerTree; j++) {
-//         tree.generateApple();
-//       }
-//       this.addTree(tree);
-//     }
-//   }
-
-//   addTree(tree) {
-//     this.trees.push(tree);
-//   }
-
-//   getCountTree() {
-//     return this.trees.length;
-//   }
-
-//   getCountApple() {
-//     return this.trees.reduce((count, tree) => count + tree.getAppleCount(), 0);
-//   }
-
-//   passOneDay() {
-//     this.age += 1;
-//     this.trees.forEach((tree) => tree.ageApples());
-
-//     if (this.age % 30 === 0) {
-//       this.trees.forEach((tree) => tree.bornApple());
-//     }
-//   }
-// }
-
-// module.exports = Garden;
-
-// const Tree = require("./Tree");
-
 class Garden {
   constructor(cntTree = 0, applesPerTree = cntTree * 2) {
     this.trees = [];
@@ -75,18 +33,3 @@ class Garden {
 }
 
 module.exports = Garden;
-
-// const myGarden = new Garden(10);
-
-// myGarden.passOneDay();
-
-// console.log(myGarden.getCountApple(), myGarden.getCountTree());
-
-// for (let i = 0; i < 10; i++) {
-//   console.log(`Дерево ${i}`);
-//   for (let j = 0; j < 20; j++) {
-//     console.log(
-//       `Яблоко ${j}: ${myGarden.trees[i].apples[j].age}, ${myGarden.trees[i].apples[j].color}, ${myGarden.trees[i].apples[j].size}`
-//     );
-//   }
-// }
